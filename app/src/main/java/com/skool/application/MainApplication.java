d717b98e05abbc0249b5e20abb46cc8733f31d80
Satender Kumar <satender.kumar@algoworks.com>
2020-01-16 20:23:08 +0530

    Creating basic structure.

---
+++ app/src/main/java/com/skool/application/MainApplication.java
@@ -0,0 +1,18 @@
+package com.skool.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Satender Kumar<satender.kumar@algoworks.com>
+ *
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
